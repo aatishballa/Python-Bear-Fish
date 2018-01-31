aa02800d1f7393c2d377fa10a3538a87be7d428f
RichardJCase <johndoe27328@gmail.com>
2018-01-31 17:15:58 -0600

    planning out animal structures

--- Animal.py
+++ Animal.py
@@ -1,5 +1,12 @@
 class Animal:
-	def __init__(self, wscreen):
+	def __init__(self, world):
+		self.world = world
 		self.wturtle = turtle.Turtle()
-		self.wscreen = wscreen
 		self.wturtle.hideturtle()
+		self.living = True
+		self.simulationSpeed = 1 
+
+	def move(self):
+		pass
+
+#liveALittle, empty, lookat, trytobreed
